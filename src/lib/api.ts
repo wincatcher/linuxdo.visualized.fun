76975ba8a0a1a7c40bf33efdc4e17fe537304e18
wincatcher <wincatcher@qq.com>
2025-01-15 19:04:15 +0800

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,11 @@
 export async function fetchAboutData() {
-    const res = await fetch('https://linux.do/about.json', { next: { revalidate: 3600 } });
-    if (!res.ok) {
-      throw new Error('获取数据失败');
-    }
-    return res.json();
+  const res = await fetch("https://linux.do/about.json", {
+    next: {
+      revalidate: 900, // 15 minutes in seconds
+    },
+  });
+  if (!res.ok) {
+    throw new Error("获取数据失败");
   }
-  
-  
+  return res.json();
+}
